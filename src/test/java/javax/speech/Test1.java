7515319f2cf11ffb5eda366c3600b17b20495dfc
Naohide Sano <namachapanda2005@gmail.com>
2023-01-31 00:44:19 +0900

    make test work

--- src/test/java/javax/speech/Test1.java
+++ src/test/java/javax/speech/Test1.java
@@ -6,16 +6,49 @@
 
 package javax.speech;
 
+import java.util.Arrays;
+import java.util.Locale;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
+import javax.speech.synthesis.Voice;
+
 import org.junit.jupiter.api.Test;
 
 
 class Test1 {
 
     @Test
-    //@EnabledIfSystemProperty(named = "vavi.test", matches = "ide")
-    void test1() throws Exception {
-        System.err.println("Hello World!");
+    void test01() throws Exception {
+        speak("She sells seashells by the seashore.");
     }
+
+    void speak(String text) throws Exception {
+        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+        SynthesizerModeDesc desc = new SynthesizerModeDesc(Locale.US);
+        Central.registerEngineCentral("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");
+        Synthesizer synthesizer = Central.createSynthesizer(desc);
+
+        synthesizer.allocate();
+        synthesizer.resume();
+
+        SynthesizerModeDesc smd = (SynthesizerModeDesc) synthesizer.getEngineModeDesc();
+System.err.println("---- voices ----");
+Arrays.stream(smd.getVoices()).forEach(v -> System.err.println(v.getName()));
+System.err.println("---");
+        String name = "kevin16";
+//        String name = "kevin";
+        Voice voice = Arrays.stream(smd.getVoices())
+                .filter(v -> v.getName().equals(name))
+                .findFirst().get();
+        synthesizer.getSynthesizerProperties().setVoice(voice);
+        synthesizer.getSynthesizerProperties().setVolume(0.5f); // 0.0 ~ 1.0
+
+        synthesizer.speakPlainText(text, null);
+
+        synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+        synthesizer.deallocate();
+    }
+
 }
 
 /* */
